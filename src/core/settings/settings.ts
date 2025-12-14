13db46833318e670c41bcfa06def366ce4b6ecc7
Alexander <sasha.kozel252000@gmail.com>
2025-12-14 21:10:28 +0300

    new homework

---
+++ src/core/settings/settings.ts
@@ -0,0 +1,9 @@
+export const SETTINGS = {
+    USERNAME: 'admin',
+    PASSWORD: 'qwerty',
+    DB: 'incubator-db',
+    //local MongoDB URL
+    MONGO_URL: 'mongodb://localhost:27017/testdb',
+    //atlas MongoDB URL
+    // MONGO_URL: 'mongodb+srv://root:12344321$@cluster0.hpu9q2g.mongodb.net/?appName=Cluster0'
+}
---
+++ src/setup-app.ts
@@ -0,0 +1,24 @@
+import express, {Request, Response} from "express";
+import {AUTH_PATH, BLOGS_PATH, POSTS_PATH, TESTING_PATH, USERS_PATH} from "./core/paths/paths";
+import {blogsRouter} from "./blogs/router/blogs-router";
+import {testingRouter} from "./testing/router/testing-router";
+import {postsRouter} from "./posts/router/postsRouter";
+import {usersRouter} from "./users/router/usersRouter";
+import {authRouter} from "./auth/router/authRouter";
+
+
+export const setupApp = () => {
+    const app = express();
+    app.use(express.json());
+
+    app.get('/', (req: Request, res: Response) => {
+        res.json({message: 'Hello World!'});
+    })
+    app.use(BLOGS_PATH, blogsRouter);
+    app.use(POSTS_PATH, postsRouter);
+    app.use(USERS_PATH, usersRouter);
+    app.use(AUTH_PATH, authRouter)
+    app.use(TESTING_PATH, testingRouter);
+
+    return app;
+}
