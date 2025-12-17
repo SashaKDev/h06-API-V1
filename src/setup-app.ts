1b9ca2c670629b535f6669e9a6a3c782716fae8e
Alexander <sasha.kozel252000@gmail.com>
2025-12-17 18:49:06 +0300

    commit

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,10 +1,11 @@
 import express, {Request, Response} from "express";
-import {AUTH_PATH, BLOGS_PATH, POSTS_PATH, TESTING_PATH, USERS_PATH} from "./core/paths/paths";
+import {AUTH_PATH, BLOGS_PATH, COMMENTS_PATH, POSTS_PATH, TESTING_PATH, USERS_PATH} from "./core/paths/paths";
 import {blogsRouter} from "./blogs/router/blogs-router";
 import {testingRouter} from "./testing/router/testing-router";
 import {postsRouter} from "./posts/router/postsRouter";
 import {usersRouter} from "./users/router/usersRouter";
 import {authRouter} from "./auth/router/authRouter";
+import {commentsRouter} from "./comments/router/commentsRouter";
 
 
 export const setupApp = () => {
@@ -19,6 +20,7 @@ export const setupApp = () => {
     app.use(USERS_PATH, usersRouter);
     app.use(AUTH_PATH, authRouter)
     app.use(TESTING_PATH, testingRouter);
+    app.use(COMMENTS_PATH, commentsRouter);
 
     return app;
 }
