c5c6b872f4d4d058dcab74a8be25e48c665ebc04
Alexander <sasha.kozel252000@gmail.com>
2025-12-17 18:48:24 +0300

    commit

--- src/auth/application/authService.ts
+++ src/auth/application/authService.ts
@@ -1,14 +1,19 @@
 import {usersRepository} from "../../users/repository/usersRepository";
 import bcrypt from "bcrypt";
+import {jwtService} from "./jwtService";
 
 export const authService = {
 
-    async checkCredentials (loginOrEmail: string, password: string): Promise<boolean> {
+    async loginUser (loginOrEmail: string, password: string): Promise<string | null> {
         const foundUser = (await usersRepository.findByLoginOrEmail(loginOrEmail))[0];
         if (!foundUser) {
-            return false;
+            return null;
         }
-        return bcrypt.compare(password, foundUser.password);
+        const passwordCheckResult = await bcrypt.compare(password, foundUser.password);
+        if (!passwordCheckResult) {
+            return null;
+        }
+        return await jwtService.createJWT(foundUser._id.toString());
     }
 
 }
---
+++ src/auth/middlewares/bearerAuthMiddleware.ts
@@ -0,0 +1,28 @@
+import {Request, Response, NextFunction} from "express";
+import {jwtService} from "../application/jwtService";
+
+export const bearerAuthMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+
+    const auth = req.headers['authorization'];
+
+    if (!auth) {
+        res.sendStatus(401);
+        return;
+    }
+
+    const authType = auth.split(' ')[0];
+    const authToken = auth.split(' ')[1];
+
+    if (authType !== 'bearer') {
+        res.sendStatus(401);
+        return;
+    }
+
+    const tokenVerifyResult = await jwtService.verifyJWT(authToken);
+    req.userId = tokenVerifyResult
+    if (!tokenVerifyResult) {
+        res.sendStatus(401);
+        return;
+    }
+    next();
+}
