37bd6b1426ad377e18af728c3ea4eee5f2f5cb5f
Alexander <sasha.kozel252000@gmail.com>
2026-02-11 21:36:00 +0300

    some refactor

--- src/core/settings/settings.ts
+++ src/core/settings/settings.ts
@@ -7,4 +7,6 @@ export const SETTINGS = {
     //atlas MongoDB URL
     // MONGO_URL: 'mongodb+srv://root:12344321$@cluster0.hpu9q2g.mongodb.net/?appName=Cluster0'
     JWT_SECRET: 'f1f5deg4hy5fr5d5g',
+    GMAIL_PASS: "hopu ihyw gllc ovnf",
+
 }
