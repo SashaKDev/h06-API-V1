132f8433a2eab0e9754c1ff3f65ae1ed54f919ff
Alexander <sasha.kozel252000@gmail.com>
2026-02-12 15:39:35 +0300

    some refactor

--- src/adapters/mailService.ts
+++ src/adapters/mailService.ts
@@ -2,7 +2,7 @@ import nodemailer from "nodemailer";
 import {SETTINGS} from "../core/settings/settings";
 
 export const mailService = {
-    async sendMail (email: string) {
+    async sendMail (email: string, confirmationCode: string) {
         const transport = nodemailer.createTransport({
             service: 'gmail',
             auth: {
@@ -15,7 +15,10 @@ export const mailService = {
             from: "sasha.val252000@gmail.com",
             to: email,
             subject: "Hello!",
-            text: "Test message"
+            text: `<h1>Thank for your registration</h1>
+                <p>To finish registration please follow the link below:
+                <a href='https://somesite.com/confirm-email?code=${confirmationCode}'>complete registration</a>
+                </p>`
         })
         console.timeEnd("send")
         console.log("Mail info:", info);
