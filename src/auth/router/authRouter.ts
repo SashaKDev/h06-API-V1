be6487b4ae618babb6cbbc185ad1c1e97c3f1bda
Alexander <sasha.kozel252000@gmail.com>
2026-02-15 22:59:21 +0300

    some refactor

--- src/auth/router/authRouter.ts
+++ src/auth/router/authRouter.ts
@@ -5,14 +5,28 @@ import {bearerAuthMiddleware} from "../middlewares/bearerAuthMiddleware";
 import {registrationHandler} from "./handlers/registrationHandler";
 import {userInputDtoValidation} from "../../users/validation/userInputDtoValidation";
 import {registrationConfirmationHandler} from "./handlers/registrationConfirmationHandler";
+import {registrationEmailResendingHandler} from "./handlers/registrationEmailResendingHandler";
+import {inputValidationResult} from "../../core/middlewares/validation/inputValidationResult";
+import {emailValidation} from "../validation/emailValidation";
 
 export const authRouter = Router();
 
-authRouter.post("/login", loginHandler);
+authRouter.post("/login",
+    loginHandler);
+
 authRouter.post("/registration",
     userInputDtoValidation,
+    inputValidationResult,
     registrationHandler)
-authRouter.post("/registration-confirmation", registrationConfirmationHandler)
+
+authRouter.post("/registration-confirmation",
+    registrationConfirmationHandler)
+
+authRouter.post("/registration-email-resending",
+    emailValidation,
+    inputValidationResult,
+    registrationEmailResendingHandler)
+
 authRouter.get("/me",
     bearerAuthMiddleware,
     getUserInfoHandler);
