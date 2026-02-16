4437f45c751398ee8ffafbe7663115c1a618eaef
Alexander <sasha.kozel252000@gmail.com>
2026-02-16 15:30:13 +0300

    some refactor

--- src/core/settings/settings.ts
+++ src/core/settings/settings.ts
@@ -3,9 +3,10 @@ export const SETTINGS = {
     PASSWORD: 'qwerty',
     DB: 'incubator-db',
     //local MongoDB URL
-    MONGO_URL: 'mongodb://localhost:27017/testdb',
+    // MONGO_URL: 'mongodb://localhost:27017/testdb',
     //atlas MongoDB URL
-    // MONGO_URL: 'mongodb+srv://root:12344321$@cluster0.hpu9q2g.mongodb.net/?appName=Cluster0'
+    MONGO_URL: 'mongodb+srv://root:root@cluster0.5oduiuj.mongodb.net/?appName=Cluster0',
+
     JWT_SECRET: 'f1f5deg4hy5fr5d5g',
     GMAIL_PASS: "hopu ihyw gllc ovnf",
 
