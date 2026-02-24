e96b5a35ddcc0f63d0b24261920c3a3e92a8e3ce
Alexander <sasha.kozel252000@gmail.com>
2026-02-24 22:17:49 +0300

    some refactor

---
+++ src/auth/router/handlers/refreshTokenHandler.ts
@@ -0,0 +1,24 @@
+import {Request, Response} from "express";
+import {authService} from "../../../adapters/authService";
+
+export const refreshTokenHandler = async (req: Request, res: Response) => {
+
+    // ВЫНЕСТИ ПРОВЕРКУ REFRESHTOKEN В МИДЛВАРУ
+
+    const oldRefreshToken = req.cookies.refreshToken;
+
+    const newTokens = await authService.refreshJWTandRefreshToken(oldRefreshToken);
+
+    if (!newTokens) {
+        res.sendStatus(401);
+        return;
+    }
+
+    res.cookie("refreshToken", newTokens.refreshToken, { httpOnly: true, secure: true });
+    res
+        .status(200)
+        .json({
+            accessToken: newTokens.refreshToken
+        });
+
+}
