09bff4b23eac41536e15d3e0067ae13b40aad564
Alexander <sasha.kozel252000@gmail.com>
2026-02-24 22:17:39 +0300

    some refactor

--- src/auth/router/handlers/loginHandler.ts
+++ src/auth/router/handlers/loginHandler.ts
@@ -6,15 +6,21 @@ export const loginHandler = async (req: Request, res: Response) => {
     const loginOrEmail = req.body.loginOrEmail;
     const password = req.body.password;
 
-    const authResultJWT = await authService.loginUser(loginOrEmail, password);
-    if (authResultJWT) {
-        res
-            .status(200)
-            .json({
-                "accessToken": authResultJWT
-            });
-    } else {
+    const authResult = await authService.loginUser(loginOrEmail, password);
+
+    if (!authResult) {
         res.sendStatus(401);
+        return;
     }
+
+    const jwtToken = authResult.jwtToken;
+    const refreshToken = authResult.refreshToken;
+
+    res.cookie("refreshToken", refreshToken, {httpOnly: true,secure: true});
+    res
+        .status(200)
+        .json({
+            accessToken: jwtToken,
+        })
 
 }
