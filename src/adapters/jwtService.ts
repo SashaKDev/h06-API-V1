8ce9a05e971858e3ee7ff14a1ee6ad714eb2fcff
Alexander <sasha.kozel252000@gmail.com>
2026-02-24 22:17:36 +0300

    some refactor

--- src/adapters/jwtService.ts
+++ src/adapters/jwtService.ts
@@ -4,8 +4,13 @@ import {SETTINGS} from "../core/settings/settings";
 export const jwtService = {
 
     async createJWT (id: string): Promise<string> {
-        const token = jwt.sign({userId: id}, SETTINGS.JWT_SECRET, {expiresIn: '1h'});
+        const token = jwt.sign({userId: id}, SETTINGS.JWT_SECRET, {expiresIn: '10s'});
         return token;
+    },
+
+    async createRefreshToken (id: string): Promise<string> {
+        const refreshToken = jwt.sign({userId: id}, SETTINGS.REFRESH_TOKEN_SECRET, {expiresIn: '20s'});
+        return refreshToken;
     },
 
     async verifyJWT (token: string): Promise<string | null> {
@@ -17,5 +22,15 @@ export const jwtService = {
             return null;
         }
 
+    },
+
+    async verifyRefreshToken (token: string): Promise<string | null> {
+        try {
+            const payload = jwt.verify(token, SETTINGS.REFRESH_TOKEN_SECRET) as { userId: string };
+            console.log(payload);
+            return payload.userId;
+        } catch (e) {
+            return null;
+        }
     }
 }
