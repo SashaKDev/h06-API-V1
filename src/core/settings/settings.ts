f96f1a79792c9db10025221b4ae03000d08ade68
Alexander <sasha.kozel252000@gmail.com>
2026-02-24 22:17:57 +0300

    some refactor

--- src/core/settings/settings.ts
+++ src/core/settings/settings.ts
@@ -9,6 +9,7 @@ export const SETTINGS = {
     // MONGO_URL: 'mongodb+srv://root:root@cluster0.5oduiuj.mongodb.net/?appName=Cluster0',
 
     JWT_SECRET: 'f1f5deg4hy5fr5d5g',
+    REFRESH_TOKEN_SECRET: 'i1y4deh2hy2rr5d5g',
     GMAIL_PASS: "hopu ihyw gllc ovnf",
 
 }
