5051c194e16766efce96da40edd2bab71cac0c83
Alexander <sasha.kozel252000@gmail.com>
2026-03-05 23:15:51 +0300

    some refactor

--- src/auth/router/handlers/logoutHandler.ts
+++ src/auth/router/handlers/logoutHandler.ts
@@ -3,10 +3,18 @@ import {jwtService} from "../../../adapters/jwtService";
 import {refreshTokenRepository} from "../../repositories/refreshTokenRepository";
 
 export const logoutHandler = async (req: Request, res: Response) => {
-
+    console.log("handler")
     const oldRefreshToken = req.cookies.refreshToken;
 
+    const findResult = await refreshTokenRepository.findRefreshToken(oldRefreshToken);
+
+    if (findResult) {
+        res.sendStatus(401);
+        return;
+    }
+
     const verifyResult = await jwtService.verifyRefreshToken(oldRefreshToken);
+    // console.log("result " + verifyResult);
     if (!verifyResult) {
         res.sendStatus(401);
         return;
