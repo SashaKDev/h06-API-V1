d1ad6963d0689216e36d47f9b7f51d7794f82578
Alexander <sasha.kozel252000@gmail.com>
2026-03-08 21:17:23 +0300

    some refactor

--- src/auth/router/handlers/loginHandler.ts
+++ src/auth/router/handlers/loginHandler.ts
@@ -1,12 +1,20 @@
 import {Request, Response} from "express";
 import {authService} from "../../../adapters/authService";
+import {getDeviceTitle} from "../../../adapters/getDeviceTitle";
 
 export const loginHandler = async (req: Request, res: Response) => {
 
     const loginOrEmail = req.body.loginOrEmail;
     const password = req.body.password;
+    const userAgent = req.headers['user-agent'];
 
-    const authResult = await authService.loginUser(loginOrEmail, password);
+    const title = getDeviceTitle(userAgent);
+    const ip = req.ip ? req.ip : "0:0:0:0";
+    console.log(title);
+    console.log(ip);
+
+
+    const authResult = await authService.loginUser(loginOrEmail, password, title, ip);
 
     if (!authResult) {
         res.sendStatus(401);
