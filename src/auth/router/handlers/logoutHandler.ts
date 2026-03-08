2c2a6aee427fd1152699eb34a28caf00a622f5c3
Alexander <sasha.kozel252000@gmail.com>
2026-03-08 21:17:10 +0300

    some refactor

--- src/auth/router/handlers/logoutHandler.ts
+++ src/auth/router/handlers/logoutHandler.ts
@@ -1,26 +1,18 @@
 import {Request, Response} from "express";
 import {jwtService} from "../../../adapters/jwtService";
 import {refreshTokenRepository} from "../../repositories/refreshTokenRepository";
+import {sessionsCollection} from "../../../db/mongo.db";
+import jwt from "jsonwebtoken";
 
 export const logoutHandler = async (req: Request, res: Response) => {
-    console.log("handler")
+
     const oldRefreshToken = req.cookies.refreshToken;
 
-    const findResult = await refreshTokenRepository.findRefreshToken(oldRefreshToken);
+    const {userId, deviceId} = jwt.decode(oldRefreshToken) as {userId: string, deviceId: string};
 
-    if (findResult) {
-        res.sendStatus(401);
-        return;
-    }
+    await sessionsCollection.deleteOne({userId: userId, deviceId: deviceId});
 
-    const verifyResult = await jwtService.verifyRefreshToken(oldRefreshToken);
-    // console.log("result " + verifyResult);
-    if (!verifyResult) {
-        res.sendStatus(401);
-        return;
-    }
-
-    await refreshTokenRepository.addTokenToBlackList(oldRefreshToken);
     res.sendStatus(204);
+
 
 }
