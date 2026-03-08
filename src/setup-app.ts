8b4f19fed26063355f516bb4e9372a2e856704c8
Alexander <sasha.kozel252000@gmail.com>
2026-03-08 21:17:36 +0300

    some refactor

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -1,5 +1,13 @@
 import express, {Request, Response} from "express";
-import {AUTH_PATH, BLOGS_PATH, COMMENTS_PATH, POSTS_PATH, TESTING_PATH, USERS_PATH} from "./core/paths/paths";
+import {
+    AUTH_PATH,
+    BLOGS_PATH,
+    COMMENTS_PATH,
+    POSTS_PATH,
+    SECURITY_DEVICES_PATH,
+    TESTING_PATH,
+    USERS_PATH
+} from "./core/paths/paths";
 import {blogsRouter} from "./blogs/router/blogs-router";
 import {testingRouter} from "./testing/router/testing-router";
 import {postsRouter} from "./posts/router/postsRouter";
@@ -7,12 +15,15 @@ import {usersRouter} from "./users/router/usersRouter";
 import {authRouter} from "./auth/router/authRouter";
 import {commentsRouter} from "./comments/router/commentsRouter";
 import cookieParser from "cookie-parser";
+import {securityDevicesRouter} from "./securityDevices/router/securityDevicesRouter";
 
 
 export const setupApp = () => {
     const app = express();
     app.use(express.json());
     app.use(cookieParser());
+
+    app.set('trust proxy', true)
 
     app.get('/', (req: Request, res: Response) => {
         res.json({message: 'Hello World!'});
@@ -23,6 +34,7 @@ export const setupApp = () => {
     app.use(AUTH_PATH, authRouter)
     app.use(TESTING_PATH, testingRouter);
     app.use(COMMENTS_PATH, commentsRouter);
+    app.use(SECURITY_DEVICES_PATH, securityDevicesRouter);
 
     return app;
 }
