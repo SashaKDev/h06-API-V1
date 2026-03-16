0b775d4ce4986975b85b0bde4d12870ccdaabbf9
Alexander <sasha.kozel252000@gmail.com>
2026-03-16 17:59:49 +0300

    some refactor

--- src/users/repository/usersRepository.ts
+++ src/users/repository/usersRepository.ts
@@ -2,24 +2,23 @@ import {User} from "../types/user";
 import {usersCollection} from "../../db/mongo.db";
 import {ObjectId, WithId} from "mongodb";
 
-export const usersRepository = {
-
+export class UsersRepository {
     async findById(id: string): Promise<WithId<User> | null> {
         console.log(id)
         const user = await usersCollection.findOne({_id: new ObjectId(id)})
         console.log(user);
         return user;
-    },
+    }
 
-    async findByConfirmationCode(confirmationCode: string): Promise< WithId<User>| null> {
+    async findByConfirmationCode(confirmationCode: string): Promise<WithId<User> | null> {
         const user = await usersCollection.findOne({"emailConfirmation.confirmationCode": confirmationCode});
         if (!user) {
             return null;
         }
         return user
-    },
+    }
 
-    async findByLoginOrEmail(loginOrEmail: string): Promise<WithId<User>[]>{
+    async findByLoginOrEmail(loginOrEmail: string): Promise<WithId<User>[]> {
         const user = await usersCollection
             .find({
                 $or: [
@@ -28,52 +27,129 @@ export const usersRepository = {
                 ]
             }).toArray();
         return user;
-    },
+    }
+
     async create(newUser: User): Promise<string> {
         console.log(111111111111111111)
         console.log(newUser);
         const insertResult = await usersCollection.insertOne(newUser);
         return insertResult.insertedId.toString();
-    },
+    }
 
     async updateConfirmationStatus(email: string): Promise<number> {
         const updateResult = await usersCollection.updateOne(
             {email: email},
-            {$set:
+            {
+                $set:
                     {
                         "emailConfirmation.isConfirmed": true
                     }
             }
         );
         return updateResult.matchedCount;
-    },
+    }
 
-    async updateConfirmationCode(email: string, newConfirmationCode: string ): Promise<number> {
+    async updateConfirmationCode(email: string, newConfirmationCode: string): Promise<number> {
         const updateResult = await usersCollection.updateOne(
             {email: email},
-            {$set:
+            {
+                $set:
                     {
                         "emailConfirmation.confirmationCode": newConfirmationCode
                     }
             }
         );
         return updateResult.matchedCount;
-    },
+    }
 
-    async deleteById (id: string): Promise<number> {
+    async deleteById(id: string): Promise<number> {
         const deleteResult = await usersCollection.deleteOne({_id: new ObjectId(id)});
         return deleteResult.deletedCount
-    },
+    }
 
     async isLoginUnique(login: string): Promise<boolean> {
-        const verificationResult = await usersCollection.
-        find({login: login}).toArray();
+        const verificationResult = await usersCollection.find({login: login}).toArray();
         return verificationResult.length > 0
-    },
+    }
 
     async isEmailUnique(email: string): Promise<boolean> {
-        const verificationResult = await usersCollection.
-        find({email: email}).toArray();
+        const verificationResult = await usersCollection.find({email: email}).toArray();
         return verificationResult.length > 0
-    },
+    }
 }
+
+// export const usersRepository = {
+//
+//     async findById(id: string): Promise<WithId<User> | null> {
+//         console.log(id)
+//         const user = await usersCollection.findOne({_id: new ObjectId(id)})
+//         console.log(user);
+//         return user;
+//     },
+//
+//     async findByConfirmationCode(confirmationCode: string): Promise< WithId<User>| null> {
+//         const user = await usersCollection.findOne({"emailConfirmation.confirmationCode": confirmationCode});
+//         if (!user) {
+//             return null;
+//         }
+//         return user
+//     },
+//
+//     async findByLoginOrEmail(loginOrEmail: string): Promise<WithId<User>[]>{
+//         const user = await usersCollection
+//             .find({
+//                 $or: [
+//                     {login: loginOrEmail},
+//                     {email: loginOrEmail}
+//                 ]
+//             }).toArray();
+//         return user;
+//     },
+//     async create(newUser: User): Promise<string> {
+//         console.log(111111111111111111)
+//         console.log(newUser);
+//         const insertResult = await usersCollection.insertOne(newUser);
+//         return insertResult.insertedId.toString();
+//     },
+//
+//     async updateConfirmationStatus(email: string): Promise<number> {
+//         const updateResult = await usersCollection.updateOne(
+//             {email: email},
+//             {$set:
+//                     {
+//                         "emailConfirmation.isConfirmed": true
+//                     }
+//             }
+//         );
+//         return updateResult.matchedCount;
+//     },
+//
+//     async updateConfirmationCode(email: string, newConfirmationCode: string ): Promise<number> {
+//         const updateResult = await usersCollection.updateOne(
+//             {email: email},
+//             {$set:
+//                     {
+//                         "emailConfirmation.confirmationCode": newConfirmationCode
+//                     }
+//             }
+//         );
+//         return updateResult.matchedCount;
+//     },
+//
+//     async deleteById (id: string): Promise<number> {
+//         const deleteResult = await usersCollection.deleteOne({_id: new ObjectId(id)});
+//         return deleteResult.deletedCount
+//     },
+//
+//     async isLoginUnique(login: string): Promise<boolean> {
+//         const verificationResult = await usersCollection.
+//         find({login: login}).toArray();
+//         return verificationResult.length > 0
+//     },
+//
+//     async isEmailUnique(email: string): Promise<boolean> {
+//         const verificationResult = await usersCollection.
+//         find({email: email}).toArray();
+//         return verificationResult.length > 0
+//     },
+// }
