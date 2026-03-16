e9fbd7f58bccf574d768b451c9c17a00db63bebc
Alexander <sasha.kozel252000@gmail.com>
2026-03-16 17:59:35 +0300

    some refactor

--- src/users/router/handlers/deleteUserHandler.ts
+++ src/users/router/handlers/deleteUserHandler.ts
@@ -1,12 +1,12 @@
-import {Request, Response} from "express";
-import {usersService} from "../../application/usersService";
-
-export const deleteUserHandler = async (req: Request, res: Response) => {
-
-    const deleteResult = await usersService.deleteById(req.params.id);
-    if (deleteResult === 0) {
-        res.sendStatus(404);
-        return;
-    }
-    res.sendStatus(204);
-}
+// import {Request, Response} from "express";
+// import {usersService} from "../../application/usersService";
+//
+// export const deleteUserHandler = async (req: Request, res: Response) => {
+//
+//     const deleteResult = await usersService.deleteById(req.params.id);
+//     if (deleteResult === 0) {
+//         res.sendStatus(404);
+//         return;
+//     }
+//     res.sendStatus(204);
+// }
