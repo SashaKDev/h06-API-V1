4225aa295362007a3a30e4dfcb3fe402778fa3c5
Alexander <sasha.kozel252000@gmail.com>
2026-03-16 17:59:47 +0300

    some refactor

--- src/users/repository/usersQueryRepository.ts
+++ src/users/repository/usersQueryRepository.ts
@@ -8,14 +8,15 @@ import {mapToUsersViewModelWithPaginator} from "../mappers/mapToUsersViewModelWi
 import {UserAuthInfo} from "../types/userAuthInfo";
 import {mapToUserAuthInfo} from "../mappers/mapToUserAuthInfo";
 
-export const usersQueryRepository = {
+export class UsersQueryRepository {
+
     async findById(id: string): Promise<UserViewModel | null> {
         const foundUser = await usersCollection.findOne({_id: new ObjectId(id)});
         if (!foundUser) {
             return null;
         }
         return mapUserToViewModel(foundUser);
-    },
+    }
 
     async findUserAuthInfo(id: string): Promise<UserAuthInfo | null> {
         const foundUser = await usersCollection.findOne({_id: new ObjectId(id)});
@@ -23,7 +24,7 @@ export const usersQueryRepository = {
             return null;
         }
         return mapToUserAuthInfo(foundUser);
-    },
+    }
 
     async findAll(data: UsersPaginationData): Promise<UserViewModelWithPaginator> {
         const skip = (data.pageNumber - 1) * data.pageSize;
@@ -33,18 +34,20 @@ export const usersQueryRepository = {
 
         let filter = {};
 
-        if(data.searchLoginTerm){
-            filter = {login: {$regex: data.searchLoginTerm, $options: 'i'} };
+        if (data.searchLoginTerm) {
+            filter = {login: {$regex: data.searchLoginTerm, $options: 'i'}};
         }
-        if(data.searchEmailTerm){
-            filter = {email: {$regex: data.searchEmailTerm, $options: 'i'} };
+        if (data.searchEmailTerm) {
+            filter = {email: {$regex: data.searchEmailTerm, $options: 'i'}};
         }
-        if(data.searchLoginTerm && data.searchEmailTerm) {
-            filter = { $or:
+        if (data.searchLoginTerm && data.searchEmailTerm) {
+            filter = {
+                $or:
                     [
-                        { login: { $regex: data.searchLoginTerm, $options: 'i' } },
-                        { email: {$regex: data.searchEmailTerm, $options: 'i'} }
-                    ] }
+                        {login: {$regex: data.searchLoginTerm, $options: 'i'}},
+                        {email: {$regex: data.searchEmailTerm, $options: 'i'}}
+                    ]
+            }
         }
 
         const foundUsers = await usersCollection
@@ -59,4 +62,58 @@ export const usersQueryRepository = {
         const foundUsersViewModel = foundUsers.map(mapUserToViewModel);
         return mapToUsersViewModelWithPaginator(foundUsersViewModel, totalCount, data.pageSize, data.pageNumber);
     }
+
 }
+
+// export const usersQueryRepository = {
+//     async findById(id: string): Promise<UserViewModel | null> {
+//         const foundUser = await usersCollection.findOne({_id: new ObjectId(id)});
+//         if (!foundUser) {
+//             return null;
+//         }
+//         return mapUserToViewModel(foundUser);
+//     },
+//
+//     async findUserAuthInfo(id: string): Promise<UserAuthInfo | null> {
+//         const foundUser = await usersCollection.findOne({_id: new ObjectId(id)});
+//         if (!foundUser) {
+//             return null;
+//         }
+//         return mapToUserAuthInfo(foundUser);
+//     },
+//
+//     async findAll(data: UsersPaginationData): Promise<UserViewModelWithPaginator> {
+//         const skip = (data.pageNumber - 1) * data.pageSize;
+//         const limit = data.pageSize;
+//         const sortBy = data.sortBy;
+//         const sortDirectionNumber = data.sortDirection === 'desc' ? -1 : 1;
+//
+//         let filter = {};
+//
+//         if(data.searchLoginTerm){
+//             filter = {login: {$regex: data.searchLoginTerm, $options: 'i'} };
+//         }
+//         if(data.searchEmailTerm){
+//             filter = {email: {$regex: data.searchEmailTerm, $options: 'i'} };
+//         }
+//         if(data.searchLoginTerm && data.searchEmailTerm) {
+//             filter = { $or:
+//                     [
+//                         { login: { $regex: data.searchLoginTerm, $options: 'i' } },
+//                         { email: {$regex: data.searchEmailTerm, $options: 'i'} }
+//                     ] }
+//         }
+//
+//         const foundUsers = await usersCollection
+//             .find(filter)
+//             .sort({[sortBy]: sortDirectionNumber})
+//             .skip(skip)
+//             .limit(limit)
+//             .toArray();
+//
+//         const totalCount = await usersCollection.countDocuments(filter);
+//         console.log(totalCount);
+//         const foundUsersViewModel = foundUsers.map(mapUserToViewModel);
+//         return mapToUsersViewModelWithPaginator(foundUsersViewModel, totalCount, data.pageSize, data.pageNumber);
+//     }
+// }
