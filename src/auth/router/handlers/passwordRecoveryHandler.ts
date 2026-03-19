dfc4e83597c1d51a7958e612721d6d0cb8b78a81
Alexander <sasha.kozel252000@gmail.com>
2026-03-19 17:33:23 +0300

    some refactor

---
+++ src/auth/router/handlers/passwordRecoveryHandler.ts
@@ -0,0 +1,10 @@
+import {Request, Response} from "express";
+import {authService} from "../../../adapters/authService";
+
+export const passwordRecoveryHandler = async (req: Request, res: Response) => {
+
+    const email = req.body.email;
+    await authService.recoverPassword(email);
+    res.sendStatus(204);
+
+}
