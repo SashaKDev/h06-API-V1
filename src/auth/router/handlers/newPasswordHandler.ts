17b69932e3015cc51b5a7b7f801d68920625e7be
Alexander <sasha.kozel252000@gmail.com>
2026-03-27 16:59:44 +0300

    some refactor

--- src/auth/router/handlers/newPasswordHandler.ts
+++ src/auth/router/handlers/newPasswordHandler.ts
@@ -1,17 +1,17 @@
-import {Request, Response} from 'express'
-import {authService} from "../../../adapters/authService";
-
-export const newPasswordHandler = async (req: Request, res: Response) => {
-
-    const newPassword = req.body.newPassword
-    const recoveryCode = req.body.recoveryCode;
-    const updateResult = await authService.updatePassword(recoveryCode, newPassword);
-    if (updateResult === null) {
-        res
-            .status(400)
-            .json({ errorsMessages: [{ message: "invalid recovery code", field: "recoveryCode" }] })
-        return;
-    }
-    res.sendStatus(204);
-
-}
+// import {Request, Response} from 'express'
+// import {authService} from "../../../adapters/authService";
+//
+// export const newPasswordHandler = async (req: Request, res: Response) => {
+//
+//     const newPassword = req.body.newPassword
+//     const recoveryCode = req.body.recoveryCode;
+//     const updateResult = await authService.updatePassword(recoveryCode, newPassword);
+//     if (updateResult === null) {
+//         res
+//             .status(400)
+//             .json({ errorsMessages: [{ message: "invalid recovery code", field: "recoveryCode" }] })
+//         return;
+//     }
+//     res.sendStatus(204);
+//
+// }
