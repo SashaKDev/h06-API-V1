49397350428f76d18dfc56d60f5d72efda6cc9ba
Alexander <sasha.kozel252000@gmail.com>
2026-03-27 16:59:19 +0300

    some refactor

--- src/auth/router/handlers/loginHandler.ts
+++ src/auth/router/handlers/loginHandler.ts
@@ -1,34 +1,34 @@
-import {Request, Response} from "express";
-import {authService} from "../../../adapters/authService";
-import {getDeviceTitle} from "../../../adapters/getDeviceTitle";
-
-export const loginHandler = async (req: Request, res: Response) => {
-
-    const loginOrEmail = req.body.loginOrEmail;
-    const password = req.body.password;
-    const userAgent = req.headers['user-agent'];
-
-    const title = getDeviceTitle(userAgent);
-    const ip = req.ip ? req.ip : "0:0:0:0";
-    console.log(title);
-    console.log(ip);
-
-
-    const authResult = await authService.loginUser(loginOrEmail, password, title, ip);
-
-    if (!authResult) {
-        res.sendStatus(401);
-        return;
-    }
-
-    const jwtToken = authResult.jwtToken;
-    const refreshToken = authResult.refreshToken;
-
-    res.cookie("refreshToken", refreshToken, {httpOnly: true,secure: true});
-    res
-        .status(200)
-        .json({
-            accessToken: jwtToken,
-        })
-
-}
+// import {Request, Response} from "express";
+// import {authService} from "../../../adapters/authService";
+// import {getDeviceTitle} from "../../../adapters/getDeviceTitle";
+//
+// export const loginHandler = async (req: Request, res: Response) => {
+//
+//     const loginOrEmail = req.body.loginOrEmail;
+//     const password = req.body.password;
+//     const userAgent = req.headers['user-agent'];
+//
+//     const title = getDeviceTitle(userAgent);
+//     const ip = req.ip ? req.ip : "0:0:0:0";
+//     console.log(title);
+//     console.log(ip);
+//
+//
+//     const authResult = await authService.loginUser(loginOrEmail, password, title, ip);
+//
+//     if (!authResult) {
+//         res.sendStatus(401);
+//         return;
+//     }
+//
+//     const jwtToken = authResult.jwtToken;
+//     const refreshToken = authResult.refreshToken;
+//
+//     res.cookie("refreshToken", refreshToken, {httpOnly: true,secure: true});
+//     res
+//         .status(200)
+//         .json({
+//             accessToken: jwtToken,
+//         })
+//
+// }
--- src/auth/router/handlers/logoutHandler.ts
+++ src/auth/router/handlers/logoutHandler.ts
@@ -1,16 +1,16 @@
-import {Request, Response} from "express";
-import {sessionsCollection} from "../../../db/mongo.db";
-import jwt from "jsonwebtoken";
-
-export const logoutHandler = async (req: Request, res: Response) => {
-
-    const oldRefreshToken = req.cookies.refreshToken;
-
-    const {userId, deviceId} = jwt.decode(oldRefreshToken) as {userId: string, deviceId: string};
-
-    await sessionsCollection.deleteOne({userId: userId, deviceId: deviceId});
-
-    res.sendStatus(204);
-
-
-}
+// import {Request, Response} from "express";
+// import {sessionsCollection} from "../../../db/mongo.db";
+// import jwt from "jsonwebtoken";
+//
+// export const logoutHandler = async (req: Request, res: Response) => {
+//
+//     const oldRefreshToken = req.cookies.refreshToken;
+//
+//     const {userId, deviceId} = jwt.decode(oldRefreshToken) as {userId: string, deviceId: string};
+//
+//     await sessionsCollection.deleteOne({userId: userId, deviceId: deviceId});
+//
+//     res.sendStatus(204);
+//
+//
+// }
