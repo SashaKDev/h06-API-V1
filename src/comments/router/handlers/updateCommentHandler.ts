e81da051097bae856533141bcbd0f7104b860626
Alexander <sasha.kozel252000@gmail.com>
2026-03-27 17:00:14 +0300

    some refactor

--- src/comments/router/handlers/updateCommentHandler.ts
+++ src/comments/router/handlers/updateCommentHandler.ts
@@ -1,20 +1,20 @@
-import {Request, Response} from "express";
-import {commentsService} from "../../application/commenstService";
-
-export const updateCommentHandler = async (req: Request, res: Response) => {
-
-    const foundComment = await commentsService.findById(req.params.id);
-    if (!foundComment) {
-        res.sendStatus(404);
-        return;
-    }
-    if (foundComment.userId !== req.userId) {
-        res.sendStatus(403);
-        return;
-    }
-    const updateResult = await commentsService.updateComment(req.params.id, req.body.content);
-    if (updateResult === 1) {
-        res.sendStatus(204);
-        return;
-    }
-}
+// import {Request, Response} from "express";
+// import {commentsService} from "../../application/commentsService.js";
+//
+// export const updateCommentHandler = async (req: Request, res: Response) => {
+//
+//     const foundComment = await commentsService.findById(req.params.id);
+//     if (!foundComment) {
+//         res.sendStatus(404);
+//         return;
+//     }
+//     if (foundComment.userId !== req.userId) {
+//         res.sendStatus(403);
+//         return;
+//     }
+//     const updateResult = await commentsService.updateComment(req.params.id, req.body.content);
+//     if (updateResult === 1) {
+//         res.sendStatus(204);
+//         return;
+//     }
+// }
