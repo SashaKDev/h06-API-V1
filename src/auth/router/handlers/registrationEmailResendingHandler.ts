a7ea2a7e8706788e4481cb4b5cba27406eb2024f
Alexander <sasha.kozel252000@gmail.com>
2026-03-27 17:00:02 +0300

    some refactor

--- src/auth/router/handlers/registrationEmailResendingHandler.ts
+++ src/auth/router/handlers/registrationEmailResendingHandler.ts
@@ -1,25 +1,25 @@
-import {Request, Response} from "express";
-import {authService} from "../../../adapters/authService";
-
-export const registrationEmailResendingHandler = async (req: Request, res: Response) => {
-
-    const email = req.body.email;
-
-    try {
-        await authService.resendConfirmationCode(email);
-    } catch (error: any)  {
-        res
-            .status(400)
-            .json({
-                "errorsMessages": [
-                    {
-                        "message": error.message,
-                        "field": "email"
-                    }
-                ]
-            })
-        return;
-    }
-    res.sendStatus(204)
-
-}
+// import {Request, Response} from "express";
+// import {authService} from "../../../adapters/authService";
+//
+// export const registrationEmailResendingHandler = async (req: Request, res: Response) => {
+//
+//     const email = req.body.email;
+//
+//     try {
+//         await authService.resendConfirmationCode(email);
+//     } catch (error: any)  {
+//         res
+//             .status(400)
+//             .json({
+//                 "errorsMessages": [
+//                     {
+//                         "message": error.message,
+//                         "field": "email"
+//                     }
+//                 ]
+//             })
+//         return;
+//     }
+//     res.sendStatus(204)
+//
+// }
--- src/auth/router/handlers/registrationHandler.ts
+++ src/auth/router/handlers/registrationHandler.ts
@@ -1,39 +1,39 @@
-import {Request, Response} from 'express';
-import {authService} from "../../../adapters/authService";
-import {mailService} from "../../../adapters/mailService";
-
-export const registrationHandler = async (req: Request, res: Response) => {
-
-    const login = req.body.login;
-    const password = req.body.password;
-    const email = req.body.email;
-
-    // НОРМАЛЬНО ЛИ ВРУЧНУЮ ОТПРАВЛЯТЬ ОШИБКУ
-    // ЕСЛИ НЕТ ТО КАК ЕЕ ЗАКИНУТЬ К ОСТАЛЬНЫМ ОШИБКАМ
-    let confirmationCode = "";
-    try {
-        confirmationCode = await authService.registerUser(login, password, email);
-    } catch (error: any) {
-        const field = error.message.split(" ")[0];
-        res
-            .status(400)
-            .json({errorsMessages: [{message: error.message, field: field}]});
-        return;
-    }
-    // if (confirmationCode === null) {
-    //     res
-    //         .status(400)
-    //         .json({
-    //             "errorsMessages": [
-    //                 {
-    //                     "message": "Email already exists",
-    //                     "field": "email"
-    //                 }
-    //             ]
-    //         })
-    //     return;
-    // }
-    await mailService.sendMail(email, confirmationCode);
-        res
-        .sendStatus(204)
-}
+// import {Request, Response} from 'express';
+// import {authService} from "../../../adapters/authService";
+// import {mailService} from "../../../adapters/mailService";
+//
+// export const registrationHandler = async (req: Request, res: Response) => {
+//
+//     const login = req.body.login;
+//     const password = req.body.password;
+//     const email = req.body.email;
+//
+//     // НОРМАЛЬНО ЛИ ВРУЧНУЮ ОТПРАВЛЯТЬ ОШИБКУ
+//     // ЕСЛИ НЕТ ТО КАК ЕЕ ЗАКИНУТЬ К ОСТАЛЬНЫМ ОШИБКАМ
+//     let confirmationCode = "";
+//     try {
+//         confirmationCode = await authService.registerUser(login, password, email);
+//     } catch (error: any) {
+//         const field = error.message.split(" ")[0];
+//         res
+//             .status(400)
+//             .json({errorsMessages: [{message: error.message, field: field}]});
+//         return;
+//     }
+//     // if (confirmationCode === null) {
+//     //     res
+//     //         .status(400)
+//     //         .json({
+//     //             "errorsMessages": [
+//     //                 {
+//     //                     "message": "Email already exists",
+//     //                     "field": "email"
+//     //                 }
+//     //             ]
+//     //         })
+//     //     return;
+//     // }
+//     await mailService.sendMail(email, confirmationCode);
+//         res
+//         .sendStatus(204)
+// }
