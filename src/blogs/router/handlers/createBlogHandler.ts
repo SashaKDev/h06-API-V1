61d935e3baf67c972d0bd1d58079b1278b4460d7
Alexander <sasha.kozel252000@gmail.com>
2026-03-27 16:58:44 +0300

    some refactor

--- src/blogs/router/handlers/createBlogHandler.ts
+++ src/blogs/router/handlers/createBlogHandler.ts
@@ -1,21 +1,21 @@
-import {Request, Response} from 'express';
-import {BlogInputDto} from "../../dto/blog-input.dto";
-import {blogsService} from "../../application/blogsService";
-import {blogsQueryRepository} from "../../repositories/blogsQueryRepository";
-
-export const createBlogHandler = async (req: Request, res: Response) => {
-
-    const newBlog: BlogInputDto = {
-        name: req.body.name,
-        description: req.body.description,
-        websiteUrl: req.body.websiteUrl,
-    }
-
-    const createdBlogId = await blogsService.create(newBlog);
-    const createdBlogViewModel = await blogsQueryRepository.findById(createdBlogId);
-
-    res
-        .status(201)
-        .json(createdBlogViewModel);
-
-}
+// import {Request, Response} from 'express';
+// import {BlogInputDto} from "../../dto/blog-input.dto.js";
+// import {blogsService} from "../../application/blogsService.js";
+// import {blogsQueryRepository} from "../../repositories/blogsQueryRepository.js";
+//
+// export const createBlogHandler = async (req: Request, res: Response) => {
+//
+//     const newBlog: BlogInputDto = {
+//         name: req.body.name,
+//         description: req.body.description,
+//         websiteUrl: req.body.websiteUrl,
+//     }
+//
+//     const createdBlogId = await blogsService.create(newBlog);
+//     const createdBlogViewModel = await blogsQueryRepository.findById(createdBlogId);
+//
+//     res
+//         .status(201)
+//         .json(createdBlogViewModel);
+//
+// }
--- src/posts/router/handlers/createCommentForPostHandler.ts
+++ src/posts/router/handlers/createCommentForPostHandler.ts
@@ -1,20 +1,20 @@
-import {Request, Response} from "express";
-import {commentsService} from "../../../comments/application/commenstService";
-import {commentsQueryRepository} from "../../../comments/repositories/commentsQueryRepository";
-
-
-export const createCommentForPostHandler = async (req: Request, res: Response) => {
-    const newComment = req.body.content
-    const postId = req.params.id
-    const userId = req.userId!;
-    const newCommentId = await commentsService.createCommentForPost(postId, newComment, userId);
-    // console.log(newCommentId + "123")
-    if (!newCommentId) {
-        res.sendStatus(404);
-        return;
-    }
-    const newCommentViewModel = await commentsQueryRepository.findById(newCommentId);
-    res
-        .status(201)
-        .json(newCommentViewModel);
-}
+// import {Request, Response} from "express";
+// import {commentsService} from "../../../comments/application/commentsService.js";
+// import {commentsQueryRepository} from "../../../comments/repositories/commentsQueryRepository.js";
+//
+//
+// export const createCommentForPostHandler = async (req: Request, res: Response) => {
+//     const newComment = req.body.content
+//     const postId = req.params.id
+//     const userId = req.userId!;
+//     const newCommentId = await commentsService.createCommentForPost(postId, newComment, userId);
+//     // console.log(newCommentId + "123")
+//     if (!newCommentId) {
+//         res.sendStatus(404);
+//         return;
+//     }
+//     const newCommentViewModel = await commentsQueryRepository.findById(newCommentId);
+//     res
+//         .status(201)
+//         .json(newCommentViewModel);
+// }
