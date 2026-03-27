20f678118dc84bf4372508766835090fab728302
Alexander <sasha.kozel252000@gmail.com>
2026-03-27 16:59:14 +0300

    some refactor

--- src/auth/router/handlers/getUserInfoHandler.ts
+++ src/auth/router/handlers/getUserInfoHandler.ts
@@ -1,13 +1,13 @@
-import {Request, Response} from "express";
-import {usersQueryRepository} from "../../../users/router/composition-root";
-
-export const getUserInfoHandler = async (req: Request, res: Response) => {
-    const userAuthInfo = await usersQueryRepository.findUserAuthInfo(req.userId!);
-    if (!userAuthInfo) {
-        res.sendStatus(404);
-        return;
-    }
-    res
-        .status(200)
-        .json(userAuthInfo);
-}
+// import {Request, Response} from "express";
+// import {usersQueryRepository} from "../../../composition-root";
+//
+// export const getUserInfoHandler = async (req: Request, res: Response) => {
+//     const userAuthInfo = await usersQueryRepository.findUserAuthInfo(req.userId!);
+//     if (!userAuthInfo) {
+//         res.sendStatus(404);
+//         return;
+//     }
+//     res
+//         .status(200)
+//         .json(userAuthInfo);
+// }
