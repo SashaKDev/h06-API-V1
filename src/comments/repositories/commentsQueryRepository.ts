188c431faeadf9a0613792e97661edc03f6b3271
Alexander <sasha.kozel252000@gmail.com>
2026-04-09 17:23:28 +0300

    some refactor

--- src/comments/repositories/commentsQueryRepository.ts
+++ src/comments/repositories/commentsQueryRepository.ts
@@ -6,33 +6,33 @@ import {CommentsPaginationData} from "../types/commentsPaginationData.js";
 import {CommentsViewModelWithPaginator} from "../types/commentsViewModelWithPaginator.js";
 import {mapToCommentsViewModelWithPaginator} from "../mappers/mapToCommentsViewModelWithPaginator.js";
 import {injectable} from "inversify";
+import {CommentModel} from "../model/commentModel.js";
 
 @injectable()
 export class CommentsQueryRepository {
 
     async findById(commentId: string): Promise<CommentsViewModel | null> {
-        const foundComment = await commentsCollection.findOne({_id: new ObjectId(commentId)});
+        const foundComment = await CommentModel.findById(commentId);
         if (!foundComment) {
             return null;
         }
         return mapCommentToViewModel(foundComment);
     }
 
-    async findCommentsForPost(postId: string, data: CommentsPaginationData): Promise<CommentsViewModelWithPaginator | null> {
+    async findCommentsForPost(postId: string, data: CommentsPaginationData): Promise<CommentsViewModelWithPaginator> {
 
         const skip = (data.pageNumber - 1) * data.pageSize;
         const limit = data.pageSize;
         const sortBy = data.sortBy;
         const sortDirectionNumber = data.sortDirection === 'desc' ? -1 : 1;
 
-        const foundComments = await commentsCollection
+        const foundComments = await CommentModel
             .find({postId: postId})
             .sort({[sortBy]: sortDirectionNumber})
             .skip(skip)
             .limit(limit)
-            .toArray();
 
-        const totalCount = await commentsCollection.countDocuments({postId: postId})
+        const totalCount = await CommentModel.countDocuments({postId: postId})
 
         const foundCommentsViewModel = foundComments.map(mapCommentToViewModel);
         return mapToCommentsViewModelWithPaginator(foundCommentsViewModel, totalCount, data.pageSize, data.pageNumber);
--- src/comments/repositories/commentsRepository.ts
+++ src/comments/repositories/commentsRepository.ts
@@ -2,35 +2,102 @@ import {commentsCollection} from "../../db/mongo.db.js";
 import {CommentType} from "../types/commentType.js";
 import {ObjectId, WithId} from "mongodb";
 import {injectable} from "inversify";
+import {CommentModel} from "../model/commentModel.js";
+import {CommentDocument} from "../types/commentDocument.js";
 
 @injectable()
 export class CommentsRepository {
 
     async createCommentForPost(dto: CommentType): Promise<string> {
-        const insertedComment = await commentsCollection.insertOne(dto);
-        return insertedComment.insertedId.toString();
+        const insertedComment = await CommentModel.insertOne(dto);
+        return insertedComment.id;
     }
 
-    async findById(id: string): Promise<WithId<CommentType> | null> {
-        return await commentsCollection.findOne({_id: new ObjectId(id)})
+    async findById(id: string): Promise<CommentDocument | null> {
+        return await CommentModel.findById(id)
     }
 
     async updateComment(commentId: string, content: string): Promise<number> {
-        const updateResult = await commentsCollection.updateOne(
-            {_id: new ObjectId(commentId)},
-            {
-                $set: {
-                    content: content,
+        const foundComment = await CommentModel.findById(commentId);
+        if (!foundComment) {
+            return 0
+        }
+        foundComment.content = content;
+        await foundComment.save();
 
-                }
-            }
-        );
-        return updateResult.matchedCount
+        // const updateResult = await commentsCollection.updateOne(
+        //     {_id: new ObjectId(commentId)},
+        //     {
+        //         $set: {
+        //             content: content,
+        //
+        //         }
+        //     }
+        // );
+        return 1
     }
 
     async delete(commentId: string): Promise<number> {
-        const deleteResult = await commentsCollection.deleteOne({_id: new ObjectId(commentId)});
+        const deleteResult = await CommentModel.deleteOne({_id: new ObjectId(commentId)});
         return deleteResult.deletedCount;
+    }
+
+    async changeLikeStatus(commentId: string, likeStatus: string): Promise<number> {
+        const foundComment = await CommentModel.findById(commentId);
+        if (!foundComment) {
+            return 0
+        }
+        if (likeStatus === 'Like') {
+            console.log("like");
+            foundComment.likesInfo.likes = foundComment.likesInfo.likes + 1;
+        }
+        if (likeStatus === 'Dislike') {
+            foundComment.likesInfo.dislikes = foundComment.likesInfo.dislikes + 1;
+        }
+        await foundComment.save();
+        return 1
+    }
+
+    async changeDislikeOnLike(commentId: string): Promise<boolean> {
+        const foundComment = await CommentModel.findById(commentId);
+        if (!foundComment) {
+            return false
+        }
+        foundComment.likesInfo.likes = foundComment.likesInfo.likes + 1;
+        foundComment.likesInfo.dislikes = foundComment.likesInfo.dislikes - 1;
+        await foundComment.save();
+        return true
+    }
+
+    async changeLikeOnDislike(commentId: string): Promise<boolean> {
+        const foundComment = await CommentModel.findById(commentId);
+        if (!foundComment) {
+            return false
+        }
+        foundComment.likesInfo.likes = foundComment.likesInfo.likes - 1;
+        foundComment.likesInfo.dislikes = foundComment.likesInfo.dislikes + 1;
+        await foundComment.save();
+        return true
+    }
+
+    async decreaseLike(commentId: string): Promise<boolean> {
+        const foundComment = await CommentModel.findById(commentId);
+        if (!foundComment) {
+            return false
+        }
+        foundComment.likesInfo.likes = foundComment.likesInfo.likes - 1;
+        await foundComment.save();
+        return true
+    }
+
+    async decreaseDislike(commentId: string): Promise<boolean> {
+        const foundComment = await CommentModel.findById(commentId);
+        if (!foundComment) {
+            return false
+        }
+        foundComment.likesInfo.dislikes = foundComment.likesInfo.dislikes - 1;
+        await foundComment.save();
+        return true
     }
 
 
