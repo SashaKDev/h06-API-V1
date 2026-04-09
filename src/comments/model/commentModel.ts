107fdb3d37d8a8fc4639ac29fc52ea8063b57a3b
Alexander <sasha.kozel252000@gmail.com>
2026-04-09 17:23:25 +0300

    some refactor

---
+++ src/comments/model/commentModel.ts
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const CommentSchema = new mongoose.Schema({
+    postId: {type: String, required: true},
+    content: {type: String, required: true},
+    createdAt: {type: String, required: true},
+    userId: {type: String, required: true},
+    userLogin: {type: String, required: true},
+    likesInfo: {
+        type:
+            {
+                likes: {type: Number, required: true},
+                dislikes: {type: Number, required: true},
+            },
+        required: true},
+})
+
+export const CommentModel = mongoose.model("comments", CommentSchema)
--- src/comments/router/handlers/commentsController.ts
+++ src/comments/router/handlers/commentsController.ts
@@ -2,19 +2,54 @@ import {Request, Response} from "express";
 import {CommentsQueryRepository} from "../../repositories/commentsQueryRepository.js";
 import {inject} from "inversify";
 import {CommentsService} from "../../application/commentsService.js";
+import {UsersRepository} from "../../../users/repository/usersRepository.js";
 
 export class CommentsController {
 
     constructor(@inject(CommentsQueryRepository) protected commentsQueryRepository: CommentsQueryRepository,
-                @inject(CommentsService) protected commentsService: CommentsService) {
+                @inject(CommentsService) protected commentsService: CommentsService,
+                @inject(UsersRepository) protected usersRepository: UsersRepository,) {
     }
 
     async getCommentById(req: Request, res: Response) {
 
-        const foundComment = await this.commentsQueryRepository.findById(req.params.id);
+        const userId = req.userId;
+        console.log(userId);
+        const commentId = req.params.id;
+
+        const foundComment = await this.commentsQueryRepository.findById(commentId);
         if (!foundComment) {
             res.sendStatus(404);
             return;
+        }
+        if (!userId) {
+            res
+                .status(200)
+                .json(foundComment)
+            return;
+        }
+        const foundUser = await this.usersRepository.findById(userId)
+        if (!foundUser) {
+            res.sendStatus(404);
+            return;
+        }
+        if (foundUser.likesInfo.likes.includes(commentId)) {
+            const foundCommentClone = structuredClone(foundComment);
+            foundCommentClone.likesInfo.myStatus = "Like"
+            console.log(foundCommentClone)
+            res
+                .status(200)
+                .json(foundCommentClone);
+            return
+        }
+        if (foundUser.likesInfo.dislikes.includes(commentId)) {
+            const foundCommentClone = structuredClone(foundComment);
+            foundCommentClone.likesInfo.myStatus = "Dislike"
+            console.log(foundCommentClone)
+            res
+                .status(200)
+                .json(foundCommentClone);
+            return
         }
         res
             .status(200)
@@ -48,6 +83,18 @@ export class CommentsController {
             return;
         }
         if (deleteResult === 0) {
+            res.sendStatus(404);
+            return;
+        }
+        res.sendStatus(204);
+    }
+
+    async changeLikeStatus(req: Request, res: Response) {
+        const commentId = req.params.id;
+        const likeStatus = req.body.likeStatus;
+        const userId = req.userId as string;
+        const changeResult = await this.commentsService.changeLikeStatus(commentId, likeStatus, userId);
+        if (!changeResult) {
             res.sendStatus(404);
             return;
         }
